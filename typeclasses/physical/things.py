53ba7ae820f95aaecb11b57e75ea97c1fda2871e
Michael J. Freidel <32282481+mfreidel@users.noreply.github.com>
2020-10-13 18:45:36 -0500

    Adds 'things.py' file in typeclasses/physical

---
+++ typeclasses/physical/things.py
@@ -0,0 +1,9 @@
+"""
+Physical Things
+===============
+
+This file contains typeclasses for customized objects in the
+game's physical realm that use the 'RealThing' object typeclass.
+"""
+
+from typeclasses.objects import RealThing
